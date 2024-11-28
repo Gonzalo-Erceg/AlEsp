58ca14ce6558d2ab70741d29ce7b6dd46aeb140c
Gonzalo-Erceg <gonzalo.erceg@hotmail.com>
2024-11-28 15:05:15 -0300

    arreglo

--- js/pay.js
+++ js/pay.js
@@ -1,4 +1,22 @@
 const token= localStorage.getItem("session")
+const modal = document.getElementById("error-modal");
+const closeModal = document.getElementById("close-modal");
+const errorMessage = document.getElementById("error-message");
+const errorTitle = document.getElementById("error-title");
+function showError(message, title) {
+    errorTitle.textContent = title || "Error"; // Título por defecto
+    errorMessage.textContent = message || "Algo salió mal. Por favor, inténtalo de nuevo."; // Mensaje por defecto
+    modal.style.display = "flex";
+  }
+  
+  
+  function hideModal() {
+      modal.style.display = "none";
+  }
+  
+  // Evento para cerrar el modal al hacer clic en la 'X'
+  closeModal.addEventListener("click", hideModal);
+  
 
 
 document.addEventListener("DOMContentLoaded",()=>{
@@ -31,5 +49,5 @@ document.querySelector("#form").addEventListener("submit",(e)=>{
         body:JSON.stringify({
             amount:parseInt(e.target.amount.value)
         })
-    }).then(res=>res.json()).then(res=>console.log(res))
+    }).then(res=>res.json()).then(res=>{showError("Tu donacion ha sido recibida con exito!","Felicidades")})
 })
